8769d99fc5b779ccdf1bfc7b21cbc25c67f29c94
Joseph Son <sms0330@gmail.com>
2021-06-15 20:36:50 -0700

    Deleted box_it.js, renamed and modified box_it_script.js v1.3

--- box_it_script/boxit.js
+++ box_it_script/boxit.js
@@ -4,7 +4,6 @@ let a = args[2];
 let b = args[3];
 let c = args[4];
 let arr;
-
 if (a === undefined && b === undefined && c === undefined) {
   arr = [''];
 } else if (c === undefined && b === undefined) {
@@ -14,58 +13,52 @@ if (a === undefined && b === undefined && c === undefined) {
 } else {
   arr = [a, b, c];
 }
-
 const topLeftCorner = '┏';
 const topRightCorner = '┓';
 const bottomLeftCorner = '┗';
 const bottomRightCorner = '┛';
 const verticalLine = '┃';
 const line = '━';
-
 function drawLine(number) {
   return line.repeat(number);
 }
-
 function drawTopBorder(number) {
   return topLeftCorner + line.repeat(number) + topRightCorner;
 }
-
 function drawMiddleBorder(number) {
   if (number === 0) {
     return verticalLine + line.repeat(number + 1) + verticalLine;
   }
   return verticalLine + line.repeat(number) + verticalLine;
 }
-
 function drawBottomBorder(number) {
   return bottomLeftCorner + line.repeat(number) + bottomRightCorner;
 }
-
 function drawBarsAround(name) {
   return `"${verticalLine}${name}${verticalLine}"`;
 }
-
 // column widths
-const colMax = Math.max(...arr.map(i => i.length));
-const col2Max = 0;
+let colMax = Math.max(...arr.map(i => i.length));
+let col2Max = 0;
 const tableWidth = colMax + col2Max;
-
 // formatting function
-function boxIt(item, end) {
+function boxIt(item, end, start) {
   // calculate padding
-  const pad = colMax - item.length;
-  const boxItem = `┃${item}${' '.repeat(pad)}${' '.repeat(col2Max)}┃`;
-
+  let pad = colMax - item.length;
+  let boxItem = `┃${item}${' '.repeat(pad)}${' '.repeat(col2Max)}┃`;
   // output
-  console.log(drawTopBorder(tableWidth));
-  console.log(boxItem);
-  console.log(drawMiddleBorder(tableWidth));
-  if (end === true) {
-    console.log(drawBottomBorder(tableWidth));
+  if (start) {
+    console.log(drawTopBorder(tableWidth));
   }
+  console.log(boxItem);
+  if (end) {
+    console.log(drawBottomBorder(tableWidth));
+  } else {
+    console.log(drawMiddleBorder(tableWidth));
+  }
+  // if (end) console.log(drawBottomBorder(tableWidth));
 }
-
 // iterate array
 for (let i = 1; i <= arr.length; i++) {
-  boxIt(arr[i - 1], i == arr.length);
+  boxIt(arr[i - 1], i == arr.length, i === 1);
 }
