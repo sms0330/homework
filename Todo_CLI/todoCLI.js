63e4f2376ea9ca32cedaa5b28804361b58dca8cd
Joseph Son <sms0330@gmail.com>
2021-07-11 00:00:29 -0700

    Added 5 functions from view to quit v0.2

--- Todo_CLI/todoCLI.js
+++ Todo_CLI/todoCLI.js
@@ -7,30 +7,49 @@ const rl = readline.createInterface({
 
 console.log('\nWelcome to Todo CLI! \n--------------------');
 
-let todoList = ['Make a homework', 'Watch a movie'];
+let todoList = [];
 
-rl.question('(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit\n>', response => {
-  if (response === 'v') {
-    if (todoList.length === 0) {
-      console.log('\nList is empty...\n');
-    } else {
-      console.log(todoList);
+function todoMenu() {
+  rl.question('(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit\n>', response => {
+    if (response === 'v') {
+      if (todoList.length === 0) {
+        console.log('\nList is empty...\n');
+      } else {
+        for (let todo of todoList) {
+          if (todo.mark) {
+            console.log(`${todoList.indexOf(todo)} [\u2713] ${todo.title}`);
+          } else {
+            console.log(`${todoList.indexOf(todo)} [] ${todo.title}`);
+          }
+        }
+      }
+      todoMenu();
+    } 
+    else if (response === 'n') {
+      rl.question('\nWhat ? \n\n>', newTodo => {
+        todoList.push({ mark: false, title: newTodo });
+        todoMenu();
+      });
+    } 
+    else if (response[0] === 'c') {
+      console.log(`\nCompleted : "${todoList[response[1]].title}"\n`);
+      todoList[response[1]].mark = true;
+      todoMenu();
+    } 
+    else if (response[0] === 'd') {
+      console.log(`\nDeleted : "${todoList[response[1]].title}"\n`);
+      todoList.pop(todoList[response[1]].title);
+      todoMenu();
+    } 
+    else if (response === 'q') {
+      console.log('See you soon! 😄');
+      rl.close();
+    } 
+    else {
+      console.log('\nError, Enter only the commands in the menu\n');
+      todoMenu();
     }
-  }
-  else if (response === 'n') {
+  });
+}
 
-  } 
-  else if (response[0] === 'c') {
-
-  }
-  else if (response[0] === 'd') {
-
-  }
-  else if (response === 'q') {
-    console.log('See you soon! 😄');
-    rl.close();
-  }else {
-      console.log(error.message)
-  }
-  
-});
+todoMenu();
