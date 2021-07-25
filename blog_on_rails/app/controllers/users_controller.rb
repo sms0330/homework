49b2581ae24f7000495747e7490976467b2c6614
Joseph Son <sms0330@gmail.com>
2021-07-24 21:49:55 -0700

    Added change password v1.0

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:edit, :update, :edit_password, :update_password]
+
   def new
     @user = User.new
   end
@@ -14,19 +16,31 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: current_user.id)
   end
 
   def update
-    @user = User.find_by(id: current_user.id)
     if @user.update params.require(:user).permit(:name, :email)
         redirect_to posts_path, notice: 'Your Name and Email Updated!'
     else
         render :edit, alert: 'Please Try Again!'
     end
+
+    user_password = params.require(:user).permit(:password_digest)
+    if @user.password = params[:user][:new_password]
+        @user.update user_password
+    else 
+        flash[:alert] = "Please Try Again!"
+    end
+  end
+
+  def edit_password
   end
 
   private
+
+  def find_user
+    @user = User.find_by(id: current_user.id)
+  end
 
   def user_params
     params.require(:user).permit(
--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :edit, :update]
 
+  get('/users/:id/edit_password', {to: 'users#edit_password', as: 'edit_password'})
+  patch('/users/:id/update_password', {to: 'users#update_password', as: 'update_password'})
+
   resource :session, only: [:new, :create, :destroy]
+
+  
 end
