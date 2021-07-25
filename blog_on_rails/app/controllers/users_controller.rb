3c4766323d2342ce429ac47d643d2b9460855d97
Joseph Son <sms0330@gmail.com>
2021-07-24 18:05:32 -0700

    Added editing users v0.9

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by(id: current_user.id)
+  end
+
+  def update
+    @user = User.find_by(id: current_user.id)
+    if @user.update params.require(:user).permit(:name, :email)
+        redirect_to posts_path, notice: 'Your Name and Email Updated!'
+    else
+        render :edit, alert: 'Please Try Again!'
+    end
+  end
+
   private
 
   def user_params
--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :users, only: [:new, :create]
-  
+  resources :users, only: [:new, :create, :edit, :update]
+
   resource :session, only: [:new, :create, :destroy]
 end
