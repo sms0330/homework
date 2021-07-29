842f4bc9226b75592e30d82d50fc0dac4a4903f4
Joseph Son <sms0330@gmail.com>
2021-07-28 22:24:51 -0700

    Fixed password change function and added alert page v1.1

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -19,17 +19,30 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update params.require(:user).permit(:name, :email)
-        redirect_to posts_path, notice: 'Your Name and Email Updated!'
+    if params[:user][:current_password].blank?
+      if @user.update params.require(:user).permit(:name, :email)
+          redirect_to posts_path, notice: 'Your Name and Email Updated!'
+      else
+          render :edit, alert: 'Please Try Again!'
+      end
     else
-        render :edit, alert: 'Please Try Again!'
-    end
-
-    user_password = params.require(:user).permit(:password_digest)
-    if @user.password = params[:user][:new_password]
-        @user.update user_password
-    else 
-        flash[:alert] = "Please Try Again!"
+      if @user&.authenticate params[:user][:current_password]
+        @user.password = params[:user][:new_password]
+        if @user.password == params[:user][:new_password_confirmation]
+            if @user.update params.require(:user).permit(:password_digest)
+              redirect_to root_path, notice: "Password Has Been Changed!"
+            else
+              flash[:alert] = "Please Try Again!"
+              render :edit_password
+            end
+        else
+            flash[:alert] = "Password Does Not Match!"
+            render :edit_password
+        end
+      else
+        flash[:alert] = "Current Password is Wrong!"
+        render :edit_password
+      end
     end
   end
 
