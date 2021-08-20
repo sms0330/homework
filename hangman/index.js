b5341656ceb4ab5fdaa345e789a12858cdfab6a8
Joseph Son <sms0330@gmail.com>
2021-08-20 01:43:47 -0700

    Added making, correct/incorrect guesses and end game

--- hangman/index.js
+++ hangman/index.js
@@ -11,4 +11,61 @@ const makeKeyboard = () => {
   
 makeKeyboard();
 
+const letter = 'STRANGER';
+const letterArr = letter.split('');
+let wrongCount = 0;
 
+const gameOver = () => {
+  alert('Better luck next time...');
+  audio = new Audio('assets/lose.mp3');
+  audio.play();
+  location.reload();
+};
+
+const checkWin = () => {
+  let winCount = 0;
+  $('.answerletter').each(function (index) {
+    if ($(this).css('display') === 'flex') {
+      winCount++;
+    }
+  });
+  if (winCount === letterArr.length) {
+    alert('Congratulations! You win!');
+    audio = new Audio('assets/win.mp3');
+    audio.play();
+  } 
+};
+
+$('.hangman_img').attr('src', 'assets/gallows.jpg');
+
+$('.letter').on('click', event => {
+  const { currentTarget } = event;
+  $(currentTarget).removeClass('bg-white').addClass('bg-danger');
+  
+  let answer = currentTarget.innerHTML;
+
+  if (letterArr.includes(answer)) {
+    $(`.answerletter:contains(${answer})`).css('display', 'flex');
+  } else {
+    wrongCount++;
+      if (wrongCount === 6) {
+      $('.hangman_img').attr('src', 'assets/gallows+head+torso+2leg+2arm.jpg');
+        gameOver();
+      } else if (wrongCount === 5) {
+      $('.hangman_img').attr('src', 'assets/gallows+head+torso+2leg+arm.jpg');
+      } else if (wrongCount === 4) {
+      $('.hangman_img').attr('src', 'assets/gallows+head+torso+2leg.jpg');
+      } else if (wrongCount === 3) {
+      $('.hangman_img').attr('src', 'assets/gallows+head+torso+leg.jpg');
+      } else if (wrongCount === 2) {
+      $('.hangman_img').attr('src', 'assets/gallows+head+torso.jpg');
+      } else if (wrongCount === 1) {
+      $('.hangman_img').attr('src', 'assets/gallows+head.jpg');
+      }
+  } checkWin();
+}); 
+
+letterArr.forEach(answer => {
+  $(`<div class="answerletterbox col-sm-1 border-bottom"><text class="answerletter"> ${answer}</text></div>`,
+  ).appendTo('.answer');
+});
